c3e8cca9f15c0ebc56992e4c027a8af94c8a6e8a
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-10-27 10:22:21 +0400

    удалил пробелы

--- src/probe4.py
+++ src/probe4.py
@@ -1,11 +1,2 @@
 
-
-
-
-
-
-
-
-
-
 dict_input = [{title: 'The Shawshank Redemption', genre: 'Drama', director: 'Frank Darabont'}, {title: 'The Godfather', genre: 'Crime', director: 'Francis Ford Coppola'}, {title: 'The Dark Knight', genre: 'Action', director: 'Christopher Nolan'}], 'Drama']
