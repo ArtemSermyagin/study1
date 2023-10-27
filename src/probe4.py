ba07a5ec55ca869d7cdc13723be7b355b6f0e25d
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-10-27 10:24:19 +0400

    добавил print

--- src/probe4.py
+++ src/probe4.py
@@ -1,2 +1,11 @@
 
+
+
+
+print("Привет")
+
+
+
+
+
 dict_input = [{title: 'The Shawshank Redemption', genre: 'Drama', director: 'Frank Darabont'}, {title: 'The Godfather', genre: 'Crime', director: 'Francis Ford Coppola'}, {title: 'The Dark Knight', genre: 'Action', director: 'Christopher Nolan'}], 'Drama']
